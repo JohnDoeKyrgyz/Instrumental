acc08a59e924f170005a91a18b69dd05b4ba2407
John Atwood <JohnDoeKyrgyz@gmail.com>
2014-10-22 15:13:45 -0400

    Trying to figure out how to use Rx

--- Server/Server/Program.fs
+++ Server/Server/Program.fs
@@ -3,38 +3,29 @@
     module Main =        
         open System
         open System.Net
+        open FSharp.Control.Reactive
         
         open Protocol
+        open System.Reactive.Concurrency
                 
         /// Groups raw readings the address of the sender
-        let readingsBySource errorHandler =
+        let readingsBySource =
 
             let readAddress (endpoint : IPEndPoint, _) = endpoint.Address.ToString()
 
-            let data, errors =
+            let data =
                 Listener.messages
-                |> Observable.ofSeq
-                |> Observable.partitionChoice
-
-            errors
-            |> Observable.add errorHandler
+                |> Observable.ofSeqOn Scheduler.Default
 
             data
-            |> Observable.partitionBy readAddress
-            |> Observable.map (fun (address, partitionedObservable) -> address, partitionedObservable |> Observable.map snd)
+            |> Observable.groupBy readAddress
+            |> Observable.map (fun group -> group.Key, group |> Observable.map snd)
 
         /// Locks on to the stream of data from the first sender in a group
         let singleSource (data : IObservable<'TKey * IObservable<'TValue>>) =
-            let result = new Event<'TValue>()
-            let increment (count, _) value = count + 1, value
-
             data
-            |> Observable.scan increment (0, Unchecked.defaultof<_>)
-            |> Observable.filter (fun (i, _) -> i = 1)
-            |> Observable.map snd
-            |> Observable.add (fun (_, vs) -> vs |> Observable.add (result.Trigger))
-
-            result.Publish :> IObservable<'TValue>
+            |> Observable.first
+            |> Observable.flatmap snd
 
         let createMessages readings =
             let timmed =
@@ -58,27 +49,8 @@
             let readSenderStream (sender, data) =
                 let messages, older = createMessages data
                 Observable.add (fun value -> printfn "%s %i %O" sender (System.Threading.Thread.CurrentThread.GetHashCode()) value) messages
-                Observable.add (fun _ -> Console.Beep(440, 10)) older            
+                Observable.add (fun _ -> Console.Beep(440, 10)) older
 
-            readingsBySource onError
-            |> Observable.add readSenderStream                               
-
-        [<EntryPoint>]
-        let main _ =
-            printfn "Listening for sensor messages %A" (System.Threading.SynchronizationContext.Current)
-                        
-            // Test 1) Read all incomming data
-            //printAllReadings()
-
-            // Test 2) Read data from first source
-            let firstSource =
-                readingsBySource onError
-                |> singleSource
-
-            firstSource
-            |> Observable.add (fun v -> printfn "%A" v)
-
-            printfn "Press any key to exit..."
-            Console.ReadKey() |> ignore
-
-            0 // return an integer exit code
+            readingsBySource
+            |> Observable.subscribeWithError readSenderStream onError
+            |> ignore
